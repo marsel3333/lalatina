a5ff9337617e3795ead7a1bc09918c4f54ccc14c
marsel3333 <marselemil004@gmail.com>
2023-09-11 19:39:36 +0600

    first commit

---
+++ bin/dart_application_1.dart
@@ -0,0 +1,23 @@
+
+
+import 'dart:io';
+
+void main (){
+
+print("Введите число");
+var a = double.parse(stdin.readLineSync()!);
+
+var summ = double.tryParse($a	*	1
+$a	*	2
+$a	*	3
+$a	*	4
+$a	*	5
+$a	*	6
+$a	*	7
+$a	*	8
+$a	*	9
+$a	*	10);
+
+print(summ);
+
+}
