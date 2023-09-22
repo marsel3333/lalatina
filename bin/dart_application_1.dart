8234f833ed420e40c3c8514be30ac54ffa704207
marsel3333 <marselemil004@gmail.com>
2023-09-22 16:50:41 +0600

    Изменения в файле bin/dart_application_1.dart

--- bin/dart_application_1.dart
+++ bin/dart_application_1.dart
@@ -1,28 +1,61 @@
 
-
-
-
 import 'dart:io';
 
 void main() {
+//.Задание №1
+// print("введите число");
+// var first = double.parse(stdin.readLineSync()!);
 
-print("введите число");
-var first = double.parse(stdin.readLineSync()!);
+// print("Введите второе число");
+// var second = double.parse(stdin.readLineSync()!);
 
-print("Введите второе число");
-var second = double.parse(stdin.readLineSync()!);
+// double summ = first + second;
+// print("Сумма $summ");
 
-var summ = first + second;
-print("Сумма $summ");
+// double minus = first - second;
+// print("Вычетания $minus");
+
+// double slojenia =first * second;
+// print("Сложение $slojenia");
+
+// double delenia = first / second;
+// print("Деление $delenia");
 
 
-var minus = first - second;
-print("Вычетания $minus");
+//Задание №2
+//print('введите число');
+//int a = int.parse(stdin.readLineSync()!);
+//print(a.isEven);
 
-double slojenia =first * second;
-print("Сложение $slojenia");
 
-double delenia = first / second;
-print("Деление $delenia");
+//Задание №3
+// print('введите число');
+// int a = int.parse(stdin.readLineSync()!);
 
+// print("$a * 1 = ${a * 1}");
+// print("$a * 2 = ${a * 2}");
+// print("$a * 3 = ${a * 3}");
+// print("$a * 4 = ${a * 4}");
+// print("$a * 5 = ${a * 5}");
+// print("$a * 6 = ${a * 6}");
+// print("$a * 7 = ${a * 7}");
+// print("$a * 8 = ${a * 8}");
+// print("$a * 9 = ${a * 9}");
+// print("$a * 10 = ${a * 10}");
+
+
+// Задание №4
+// print("Введите градус в цельсиях");
+// double gradus = double.parse(stdin.readLineSync()!);
+// print("$gradus градусов в Цельсиях = ${(gradus * 9 / 5)+32} градусам в Фаренгейтах" );
+
+
+// Задание №5
+// print("Введите деньги");
+// double com = double.parse(stdin.readLineSync()!);
+// print("Введите валюту USD/nEUR/nRUB/KZT ");
+// String valuta = stdin.readLineSync()!;
+// print("Введите стоимость валюты");
+// double stoimost = double.parse(stdin.readLineSync()!);
+// print("$com $valuta = ${com * stoimost} сом");
 }
